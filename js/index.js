da414a4cdcc689803f3b51683a34ba177c58b527
chanbasha-12 <skchanbasha1111@gmail.com>
2021-02-04 18:32:00 +0530

    added git commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var container=document.querySelector("#root");
+var card=document.createElement("div");
+card.setAttribute("id0","card");
+container.appendChild(card);
+var image=document.createElement("img");
+image.src="picture/avatar.svg";
+image.alt="male";
+image.style.width="20%";
+card.appendChild(image);
